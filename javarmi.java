ab77380c122e9cb64e8295a8a328010a695731b9
Felipe Avelino <avelino.felipe@gmail.com>
2022-05-16 23:28:37 -0400

    grfsoijnrgfiojrwfgeioj

--- javarmi.java
+++ javarmi.java
@@ -1,5 +1,8 @@
 import java.rmi.*;
 import java.net.*;
+import java.rmi.registry.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.*;
 
 /*
 Servidor + Servidor de nome + Cliente
@@ -9,15 +12,58 @@ Servidor + Servente do servidor + Cliente + Servente do cliente
 + Interface do Servidor + Interface do cliente
 */
 
+public class App {
+    public static void main(String[] args) {
+        String opcao = null;
+        Scanner scan = new Scanner(System.in);
+        int id; 
+        Boolean boolCliente = false;
 
+        System.out.println("Atividade 2 - Java RMI\nby Felipe Avelino\n\n===================================\n");
+        
+        while(true){
+            System.out.println("Favor escolher uma das duas opcoes\n1: Cliente\n2: Servidor");
+            opcao = scan.nextLine();
+            if (opcao.equals("1")) {
+                boolCliente = true;
+                break;
+            }else if (opcao.equals("2")){
+                break;
+            }
+            System.out.println("Opcao invalida");
+        }
 
-public interface Interface_CriticalSection extends Remote {
-    void requestCS () throws RemoteException;
+        if (boolCliente == true) {
+            while(true){
+                System.out.println("Favor escolher um dos id's\n1, 2, 3 ou 4\n");
+                opcao = scan.nextLine();
+                id = Integer.parseInt(opcao);
+                if (id > 0 && id <5) {
+                    break;
+                }
+                System.out.println("Opcao invalida");
+            }
+
+            Client cliente = new Client(id);
+
+            while (true) {
+
+            }
+
+        }else{
+
+        }
+    }
+} 
+
+//uma interface remota 
+public interface Interface_Resource extends Remote {
+      requestCS () throws RemoteException;
     void exitCS () throws RemoteException;
 } 
 
-public class CriticalSection implements Interface_CriticalSection {
-    public CriticalSection () {
+public class Resource implements Interface_Resource {
+    public Resource () {
         //construtor
     }
 
@@ -31,10 +77,15 @@ public class CriticalSection implements Interface_CriticalSection {
 
 } 
 
-public class Servidor {
+public interface Interface_Server {
+    void registrarInteresse();
+    void cancelarInteresse();
+}
+
+public class Server {
     public void run () {
         try {
-            Interface_CriticalSection cs = new CriticalSection();
+            Interface_Resource cs = new Resource();
             String objName = "rmi://localhost/cs";
 
             //registrando o objeto no RMIRegistry
@@ -49,15 +100,31 @@ public class Servidor {
 
 } 
 
-public class Cliente {
-    public void run () {
+//servente do servidor
+public class ServImpl extends UnicastRemoteObject implements Interface_Server{
+    public ServImpl() throws RemoteException {}
+    
+    void registrarInteresse();
+}
 
+public interface Interface_Client extends Remote {
+    
+    //nao pode usar o nome notify
+    void notificar(String texto) throws RemoteException;
+}  
+
+public class Client {
+    int id; 
+
+    public Client (int id) {
+        this.id = id;
+    }
+
+    public void run () throws Exception {
+        String objName = "rmi://localhost:1099/cs";
+        Interface_Resource resource = (Interface_Resource) Naming.lookup(objName);
+        System.out.println("Client id: "+ this.id +" tentando usar o recurso!");
+        resource.requestCS();
     }
 }
 
-public class App {
-    public static void main(String[] args) {
-
-        //baseado nos parametros passados pelo terminal eu escolho se sou cliente ou servidor
-    }
-} 
