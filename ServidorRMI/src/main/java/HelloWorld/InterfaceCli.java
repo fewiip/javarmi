8b10319758fba38c66967ad6e76aa6e4266f0f26
Felipe Avelino <avelino.felipe@gmail.com>
2022-05-17 16:11:48 -0300

    Colocando os projetos

---
+++ ServidorRMI/src/main/java/HelloWorld/InterfaceCli.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package HelloWorld;
+
+import java.rmi.*;
+import java.net.*;
+/**
+ *
+ * @author felipe
+ */
+public interface InterfaceCli extends Remote {
+    void Notificar(String texto);
+}
---
+++ ServidorRMI/src/main/java/HelloWorld/InterfaceServ.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package HelloWorld;
+
+import java.rmi.*;
+import java.net.*;
+/**
+ *
+ * @author felipe
+ */
+public interface InterfaceServ extends Remote {
+    void registrarInteresse(String texto, InterfaceCli referenciaCliente);
+}
